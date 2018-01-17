626843b6754c828787cb3aa0cc72844b23b8364f
Taqin <taqin@megentics.com>
2018-01-17 15:02:03 +0800

    Added Hello World

--- app.js
+++ app.js
@@ -2,10 +2,20 @@
 const fs = require('fs');
 const os = require('os');
 
-var user = os.userInfo();
+const http = require('http');
 
-console.log(user.username);
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+}).listen(8080); //the server object listens on port 8080
+Run example »
 
-fs.appendFileSync('greeting.txt', `Hello Wordly fuckers! from ${user.username} Hahahaha`);
+
+//var user = os.userInfo();
+
+// console.log(user.username);
+
+// fs.appendFileSync('greeting.txt', `Hello Wordly fuckers! from ${user.username} Hahahaha`);
 
 // console.log('Hello Worldly Fuckers');
