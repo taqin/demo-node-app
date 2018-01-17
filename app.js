4bd49461a768a07d4f62f8258996a0bdecc9268f
Taqin <taqin@megentics.com>
2018-01-17 15:25:56 +0800

    Added NPM start script

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 // Create new file to app
-const fs = require('fs');
-const os = require('os');
+// const fs = require('fs');
+// const os = require('os');
 
 const http = require('http');
 
-//create a server object:
-http.createServer(function (req, res) {
-  res.write('Hello World!'); //write a response to the client
-  res.end(); //end the response
-}).listen(8080); //the server object listens on port 8080
-Run example »
+// create a server object:
+http
+  .createServer((req, res) => {
+    res.write('Hello World!'); // write a response to the client
+    res.end(); // end the response
+  })
+  .listen(5000); // the server object listens on port 8080
 
-
-//var user = os.userInfo();
+// var user = os.userInfo();
 
 // console.log(user.username);
 
